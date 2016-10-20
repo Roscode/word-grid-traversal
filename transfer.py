c54115b3b9ebba832ea6c1944351e1a48260eba4
Darren Roscoe <darrenroscoe@gmail.com>
2016-10-20 13:06:57 -0400

    deleted old words file and transer program

--- transfer.py
+++
@@ -1,6 +0,0 @@
-with open('words.csv', 'r') as old, open('new_words.txt', 'w') as new:
-  words_file_lines = old.readlines()
-  words = [col[1][6:].lower() for col in [line.split(',') for line in words_file_lines]]
-  for w in words:
-    new.write(w)
-    new.write('\n')
